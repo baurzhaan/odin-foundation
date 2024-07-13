34ed41cba48484954475fa42d14ab433320b9bff
Baurzhaan <102077363+baurzhaan@users.noreply.github.com>
2024-07-13 19:53:46 +0100

    generate new size game board

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,42 +1,50 @@
+function addDivs(noOfSquares = 16, container) {
+  let gameBoardSize = noOfSquares * noOfSquares;
+  for (let i = 0; i < gameBoardSize; i++) {
+    let div = document.createElement("div");
+    div.className = "div_cell";
+    container.append(div);
+  };
+
+  let div_cells = document.getElementsByClassName("div_cell");
+  for (div_cell of div_cells) {
+    div_cell.style.aspectRatio = "1/1";
+    div_cell.style.width = "100%";
+    div_cell.style.height = "100%";
+    div_cell.style.boxSizing = "border-box";
+    div_cell.style.border = "1px solid gray";
+    div_cell.style.width = `${100 / noOfSquares}%`;
+    div_cell.addEventListener("mouseover", function() {
+      this.style.backgroundColor = "blue";
+    });
+  };
+};
+
 let noOfSquares = 16;
 
-
 const btnSetNoOfSquares = document.createElement("button");
-btnSetNoOfSquares.textContent = "Set Number of squares";
+btnSetNoOfSquares.textContent = "Set size of game board (< 100)";
 btnSetNoOfSquares.id = "btnSetNoOfSquares";
+btnSetNoOfSquares.style.backgroundColor = "yellow";
+btnSetNoOfSquares.style.margin = "10px";
+btnSetNoOfSquares.style.fontSize = "20px";
 
 btnSetNoOfSquares.addEventListener("click", function(){
-  noOfSquares = prompt("Enter number of squares", noOfSquares);
+  noOfSquares = prompt("Enter number of squares", 16);
+  if (noOfSquares > 100) noOfSquares = 100;
+  if (typeof(noOfSquares) == 'string')  {
+    container.replaceChildren();
+    addDivs(noOfSquares, container);
+  } 
+  console.log(typeof(noOfSquares));
 });
 
 document.body.prepend(btnSetNoOfSquares);
 
 container = document.getElementById("container");
-
 container.style.display = "flex";
 container.style.flexFlow = "row wrap";
 container.style.justifyContent = "space-around";
 container.style.textAlign = "center";
 
-let gameBoardSize = noOfSquares * noOfSquares;
-for (let i = 0; i < gameBoardSize; i++) {
-  let div = document.createElement("div");
-  div.className = "div_cell";
-  container.append(div);
-};
-
-let div_cells = document.getElementsByClassName("div_cell");
-for (div_cell of div_cells) {
-  div_cell.style.aspectRatio = "1/1";
-  div_cell.style.width = "100%";
-  div_cell.style.height = "100%";
-  div_cell.style.boxSizing = "border-box";
-  div_cell.style.border = "1px solid gray";
-  div_cell.style.width = `${100 / noOfSquares}%`;
-  // div_cell.onmouseover = function() {
-  //     this.style.backgroundColor = "blue";
-  // };
-  div_cell.addEventListener("mouseover", function() {
-    this.style.backgroundColor = "blue"
-  });
-};
+addDivs(noOfSquares, container);
