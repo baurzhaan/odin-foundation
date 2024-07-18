b257c8b24e5facdaf001b3e70cf6e3e8aa93d82e
Baurzhaan <102077363+baurzhaan@users.noreply.github.com>
2024-07-18 19:20:37 +0100

    create operate function

--- calculator-project/script.js
+++ calculator-project/script.js
@@ -1,31 +1,46 @@
-function add(number1, number2) {
-  if (typeof(number1) === "number" && typeof(number2) === "number") return number1 + number2;
-  return "error in =add= function";
+let number1 = 1, number2 = 2, operation = '+';
+
+function operate(operator = '+', number1 = number1, number2 = number2){
+  function add(number1, number2) {
+    if (typeof(number1) === "number" && typeof(number2) === "number") return number1 + number2;
+    return "error in =add= function";
+  };
+  function subtract(number1, number2) {
+    if (typeof(number1) === "number" && typeof(number2) === "number") return number1 - number2;
+    return "error in =subtract= function";
+  };
+  function multiply(number1, number2) {
+    if (typeof(number1) === "number" && typeof(number2) === "number") return number1 * number2;
+    return "error in =multiply= function";
+  };
+  function divide(number1, number2) {
+    if (typeof(number1) === "number" && typeof(number2) === "number") return number1 / number2;
+    return "error in =divide= function";
+  };
+
+  let result = 0;
+
+  switch(operator) {
+    case '+':
+      result = add(number1, number2);
+      break;
+    case '-':
+      result = subtract(number1, number2);
+      break;
+    case '*':
+      result = multiply(number1, number2);
+      break;
+    case '/':
+      result = divide(number1, number2);
+      break;
+    default:
+      throw new Error('Unkown mathematical operator!');
+  };
+  return result;
 };
 
-function subtract(number1, number2) {
-  if (typeof(number1) === "number" && typeof(number2) === "number") return number1 - number2;
-  return "error in =subtract= function";
-};
+console.log(number1);
+console.log(number2);
+console.log(operation);
 
-function multiply(number1, number2) {
-  if (typeof(number1) === "number" && typeof(number2) === "number") return number1 * number2;
-  return "error in =multiply= function";
-};
-
-function divide(number1, number2) {
-  if (typeof(number1) === "number" && typeof(number2) === "number") return number1 / number2;
-  return "error in =divide= function";
-};
-
-console.log(add(1,2));
-console.log(add("a", "b"));
-
-console.log(subtract(1,2));
-console.log(subtract("a", "b"));
-
-console.log(multiply(1,2));
-console.log(multiply("a", "b"));
-
-console.log(divide(1,2));
-console.log(divide("a", "b"));
+console.log(operate(operation, number1, number2));
